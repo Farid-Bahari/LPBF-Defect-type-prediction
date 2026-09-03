b93a481f4235ca01b0e2f661c07f3e25d38f23aa
Farid Bahari-Sambran <162224177+Farid-Bahari@users.noreply.github.com>
2026-09-03 17:34:23 +0200

    Update Defect type prediction.py

--- Defect type prediction.py
+++ Defect type prediction.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 # --- Page Configuration ---
 st.set_page_config(
-    page_title="Melt Pool Prediction App",
+    page_title="Optimize the LPBF processing parameters",
     page_icon="🔥",
     layout="wide",
     initial_sidebar_state="expanded"

caa93f943fa4345d4395726f1034e4b3ed2a0f18
Farid Bahari-Sambran <162224177+Farid-Bahari@users.noreply.github.com>
2026-09-03 17:38:36 +0200

    Update Defect type prediction.py

--- Defect type prediction.py
+++ Defect type prediction.py
@@ -35,7 +35,7 @@ if "history" not in st.session_state:
     st.session_state.history = None
 
 # --- Application Header ---
-st.title("🔥 Melt Pool Prediction App")
+st.title("🔥 Optimize the LPBF processing parameters")
 st.markdown("### *Hybrid FEM + Machine Learning Framework*")
 st.write("---")
 
